7c8b345f4db1f8a2a1b1883dec729617bb0251a7
Narendra Samtani <alenway97@gmail.com>
2025-01-06 22:20:34 +0530

    Created variables and contatns files
    - created variables constants java file to understant basic concepts

---
+++ java_foundations/core_java_basics/variables and constants/VariablesConstants.java
@@ -0,0 +1,18 @@
+public class VariablesConstants{
+    public static void main(String[] args){
+        // Variables
+        int age = 25;
+        double salary = 50000.50;
+        String name = "John Doe";
+
+        // Constants (use 'final' keyword)
+        final double PI = 3.14159;
+        final int MAX_USERS = 100;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: $" + salary);
+        System.out.println("PI: " + PI);
+        System.out.println("Max Users: " + MAX_USERS);
+    }
+}
