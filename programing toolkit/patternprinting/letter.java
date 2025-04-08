fe90cd1e0d450673e4fe1ae2c59d18262cd94933
Narendra Samtani <alenway97@gmail.com>
2025-04-08 22:58:57 +0530

    letter finding

---
+++ programing toolkit/patternprinting/letter.java
@@ -0,0 +1,8 @@
+public class letter {
+    public static void main(String[] args) {
+        String str = "ABCDE";
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.substring(0, i));
+        }
+    }
+}
