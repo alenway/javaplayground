7baf1581fa171c4000d880e551d8afbbf7fb1c26
Narendra Samtani <alenway97@gmail.com>
2025-10-01 00:23:09 +0530

    linkedlist

--- dsa/linkedlist/LinkNode.java
+++ dsa/linkedlist/LinkNode.java
@@ -1,5 +1,14 @@
-package dsa.linkedlist;
+import java.util.LinkedList;
+
+class demo {
+    public void test() {
+        System.out.println("demo class");
+    }
+}
 
 public class LinkNode {
-
+    public static void main(String[] args) {
+        demo d1 = new demo();
+        d1.test();
+    }
 }
